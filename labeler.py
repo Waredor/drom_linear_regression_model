eda4efc697d102a9e55405ae9a74c89212a87851
Vitaly Proshutinsky <114699500+Waredor@users.noreply.github.com>
2025-03-22 21:53:16 +0300

    new ML model
    added multiple linear regression model

--- labeler.py
+++ labeler.py
@@ -10,9 +10,9 @@ with open('prius_drom_khv.json', 'r', encoding='utf-8') as file:
 for el in data_khv:
     for val in el.values():
         price = val[0]['price']
-        odo = val[1]['odo']
+        odo = int(val[1]['odo'])
         year = val[2]['year']
-        if year is not None:
+        if year is not None and odo > 80000:
             annotated_el = {'price': price, 'odo': odo, 'year': year, 'khv': 1, 'vdk': 0, 'blg': 0}
             annotated_data.append(annotated_el)
 
@@ -22,9 +22,9 @@ with open('prius_drom_vdk.json', 'r', encoding='utf-8') as file:
 for el in data_vdk:
     for val in el.values():
         price = val[0]['price']
-        odo = val[1]['odo']
+        odo = int(val[1]['odo'])
         year = val[2]['year']
-        if year is not None:
+        if year is not None and odo > 80000:
             annotated_el = {'price': price, 'odo': odo, 'year': year, 'khv': 0, 'vdk': 1, 'blg': 0}
             annotated_data.append(annotated_el)
 
@@ -34,9 +34,9 @@ with open('prius_drom_blg.json', 'r', encoding='utf-8') as file:
 for el in data_blg:
     for val in el.values():
         price = val[0]['price']
-        odo = val[1]['odo']
+        odo = int(val[1]['odo'])
         year = val[2]['year']
-        if year is not None:
+        if year is not None and odo > 80000:
             annotated_el = {'price': price, 'odo': odo, 'year': year, 'khv': 0, 'vdk': 0, 'blg': 1}
             annotated_data.append(annotated_el)
 
@@ -45,4 +45,10 @@ with open('annotated_data.json', 'w') as file:
     json.dump(annotated_data, file, ensure_ascii=False)
 
 df = pd.read_json('annotated_data.json')
+mean = df['price'].mean()
+std = df['price'].std()
+print(mean, std)
+threshold = 2
+#df = df[(df['price'] < mean + threshold * std) & (df['price'] > mean - threshold * std)]
+print(df)
 df.to_csv('annotated_data.csv', index=False)
--- linear_regression.py
+++ linear_regression.py
@@ -5,28 +5,52 @@ from sklearn.model_selection import cross_val_score
 import joblib
 
 df = pd.read_csv('annotated_data.csv')
-odo = df.iloc[:,1:2]
-target = df.iloc[:,0:1]
+X = df.loc[:,['odo', 'khv', 'vdk', 'blg']].values
+odo = df.iloc[:,1:2].values
+target = df.iloc[:,0:1].values
+
+#print(df)
+#simple lr non std
 simple_lr = LinearRegression()
-
-simple_lr.fit(odo, target)
-
-predicted_y = simple_lr.predict(odo)
-
-plt.figure(figsize=(10, 6))
-plt.scatter(odo, target)
-plt.plot(odo, predicted_y, c = 'r')
-plt.title('Scatter plot and a Simple Linear Regression Model')
-plt.ylabel("price")
-plt.xlabel("odo")
-plt.show()
 
 mse = cross_val_score(simple_lr,
                       odo,
                       target,
                       scoring='neg_mean_squared_error',
-                      cv=10)
+                      cv=5)
 
-#print(mse.mean())
+print(f'non std_simple lr: {mse.mean()}')
+
+simple_lr.fit(odo, target)
+predicted_y = simple_lr.predict(odo)
+
+plt.figure(figsize=(10, 6))
+plt.scatter(odo, target)
+plt.plot(odo, predicted_y, c = 'r')
+plt.title('Scatter plot and a linear Regression Model')
+plt.ylabel("price")
+plt.xlabel("odo")
+plt.show()
+
+#multiple lr non std
+mp_lr = LinearRegression()
+mp_lr.fit(X, target)
+
+mse = cross_val_score(mp_lr,
+                     X,
+                     target,
+                     scoring='neg_mean_squared_error',
+                     cv=5)
+
+print(f'non std_multiple lr: {mse.mean()}')
+
+y_mp = mp_lr.predict([[200000,0,1,0]])
+print(y_mp)
+
+
+#Сохраняем модели в файл
+
+#joblib.dump(mp_lr, 'linear_ML_models/linear_regression_multiple.pkl')
 #joblib.dump(simple_lr, 'linear_ML_models/linear_regression_odo.pkl')
+
 
